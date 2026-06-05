18db42230b568e303be2595dacf6ba90a976c0a3
SeregaBandit2012 <Foleon17@ya.ru>
2026-06-05 23:31:17 +0300

    end

--- CPP/Client/Pages/Driver/Vehicle1.razor.cs
+++ CPP/Client/Pages/Driver/Vehicle1.razor.cs
@@ -29,4 +29,20 @@ public partial class Vehicle1
         "Грузовой" => "🚛",
         _ => "🚘"
     };
+    private string GetImage(int vehicle_id)
+    {
+    var key = $"{vehicle_id}".ToLower();
+
+    return key switch
+    {
+        var k when k.Contains("1") => "/images/toyota.jpg",
+        var k when k.Contains("2") => "/images/solaris.jpg",
+        var k when k.Contains("3") => "/images/rio.jpg",
+        var k when k.Contains("4") => "/images/fordfocus.jpg",
+        var k when k.Contains("5") => "/images/GAZ.jpg",
+        var k when k.Contains("6") => "/images/MAN.jpg",
+        var k when k.Contains("7") => "/images/cat.jpg",
+        _ => "/images/hamam.jpg",
+    };
+}
 }
